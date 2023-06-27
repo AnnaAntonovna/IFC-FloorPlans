21dbb83e88abc94d708a617d970d70906afda8c7
Anna <a.baranova@strategiedigitali.io>
2023-06-27 16:44:54 +0200

    No plans

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ function toogleShadow(active){
   }
 }
 
-loadIfc('./02.ifc'); 
+loadIfc('./01.ifc'); 
 
 function togglePostProduction(active) {
   viewer.context.renderer.postProduction.active = active;
